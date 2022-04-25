4442dd3f1669f50539955096a9fb329f8b4bcf4e
Yellow10041 <40462438+Yellow10041@users.noreply.github.com>
2022-04-25 14:25:17 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import './App.css';
+import { useState } from "react"
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import ProductList from './components/ProductList/ProductList';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import AddProduct from './components/AddProduct/AddProduct';
+import EditProduct from './components/EditProduct/EditProduct';
+
+function App() {
+const [products, setProducts] = useState([
+{id: 1, title: 'Product 1', price: 899},
+{id: 2, title: 'Product 2', price: 982},
+{id: 3, title: 'Product 3', price: 322},
+{id: 4, title: 'Product 4', price: 763},
+{id: 5, title: 'Product 5', price: 389}
+]);
+
+const deleteProduct = (productId) => {
+const newProducts = products.filter(product => product.id !== productId);
+setProducts(newProducts);
+}
+
+const [title, setTitle] = useState("Hi student!");
+
+
+const clickMe = () => {
+setTitle("This title is changed!")
+console.log("Clicked button student ");
+}
+
+
+return (
+<div>
+<BrowserRouter>
+<Routes>
+<Route path="/" element={<ProductList products={ products } deleteProduct = { deleteProduct } /> } >
+
+</Route>
+<Route path="/about" element={<About />}>
+
+</Route>
+<Route path="/contact" element={<Contact />}>
+
+</Route>
+<Route path="/add" element={<AddProduct />}>
+
+</Route>
+<Route path="/edit/:id" element={<EditProduct />}>
+
+</Route>
+</Routes>
+</BrowserRouter>
+</div>
+);
+}
+
+export default App;
